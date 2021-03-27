86026e3259ab33b2391b15f6aad3330f3d167cce
Philipmikmik <65279994+Philipmikmik@users.noreply.github.com>
2021-03-27 15:42:08 +0100

    Add files via upload

---
+++ ntkinter-api.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi import FastAPI
+import hentai
+
+app = FastAPI(docs_url=None, redoc_url=None)
+
+@app.get("/json/")
+async def read_item():
+    comic = hentai.Utils.get_random_hentai()
+    ok = {
+        'title': comic.title(hentai.Format.Pretty),
+        'author': [artist.name for artist in comic.artist],
+        'tags': [tag.name for tag in comic.tag],
+        'img': comic.image_urls
+    }
+    return ok
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0")
