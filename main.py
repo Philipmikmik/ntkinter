5973f231d0f22f26d1cc9dceaabf0207aed0c126
Philipmikmik <65279994+Philipmikmik@users.noreply.github.com>
2021-04-27 15:58:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,20 +24,19 @@ async def read_item(exc, inc):
                     found = True
                 except IndexError:
                     maxran = ran
-        extags = exc.split(', ')
-        while True:
-            if found == False:
-                comic = hentai.Utils.get_random_hentai()
-            if (bool(set(extags) & set([tag.name for tag in comic.tag])) == False):
-                break
+        if exc != "None":
+            extags = exc.split(', ')
+            while True:
+                if found == False:
+                    comic = hentai.Utils.get_random_hentai()
+                if (bool(set(extags) & set([tag.name for tag in comic.tag])) == False):
+                    break
     ok = {
         'title': comic.title(hentai.Format.Pretty),
         'author': [artist.name for artist in comic.artist],
         'tags': [tag.name for tag in comic.tag],
         'img': comic.image_urls,
-        'id': comic.id,
-        'exclusions': exc,
-        'inclusions': inc
+        'id': comic.id
     }
     return ok
 if __name__ == "__main__":
